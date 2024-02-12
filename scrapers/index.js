07b7c97b538e8e9fe6afc0af6e7256d46ea07fa5
Abraham Serena <abraham.serena@hotmail.com>
2024-02-11 20:51:26 -0500

    Add scrapers for menu and menu links

---
+++ scrapers/index.js
@@ -0,0 +1,2 @@
+export { scrapeMenu } from './scrapeMenu.js'
+export { scrapeMenuLinksByDate } from './scrapeUrls.js'
---
+++ scrapers/scrapeUrls.js
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer'
+import { weekMenuUrl } from '../config.js'
+
+/**
+ * Scrapes menu links from a specific date's page on the week menu website.
+ * Optionally logs messages from the page's console to the Node.js console.
+ *
+ * @param {string} date - The specific date to scrape menu links for, in 'YYYY-MM-DD' format.
+ * @returns {Promise<Array<{href: string, text: string}>>} - A promise that resolves to an array of objects,
+ * each containing the href and text of a menu link.
+ */
+export const scrapeMenuLinksByDate = async (date) => {
+  const url = `${weekMenuUrl}/${date}`
+  let browser
+  try {
+    browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    const response = await page.goto(url, { waitUntil: 'networkidle2' })
+    if (!response || response.status() !== 200) {
+      throw new Error(`Page not found or error loading page: ${url}`)
+    }
+
+    const links = await page.evaluate(() => {
+      const anchors = Array.from(document.querySelectorAll('a.h-full'))
+      if (anchors.length === 0) {
+        console.warn('No anchor tags found with the class h-full.')
+        return []
+      }
+      return anchors.map(anchor => anchor.href)
+    })
+
+    return links
+  } catch (error) {
+    console.error(`Error scraping ${url}:`, error)
+    return []
+  } finally {
+    await browser?.close()
+  }
+}
