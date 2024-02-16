5fb2ad057c3b81a02f1132eaa4719252ed4fb293
Abraham Serena <abraham.serena@hotmail.com>
2024-02-15 22:59:22 -0500

    Handle new table of nutritions facts

--- scrapers/scrapeMenu.js
+++ scrapers/scrapeMenu.js
@@ -1,4 +1,75 @@
-import puppeteer from 'puppeteer'
+import { initializeAndNavigate } from '../utils/index.js'
+
+/**
+ * Scrapes menu data from a given URL.
+ * It navigates to the URL, iterates through buttons within a swiper-slide container,
+ * clicks each button, waits for the content to update, and extracts nutritional information.
+ * |--------------------|
+ * | Nutrition Facts    |
+ * | Per bowl           |
+ * |--------------------|
+ * | Calories 650       |
+ * | Fat 30g            |
+ * | ...                |
+ * |____________________|
+ *
+ * @param {string} url - The URL of the page to scrape data from.
+ * @returns {Promise<Object|null>} A promise that resolves to an object containing the meal's ID, name, URL, and servings data,
+ * including nutritional facts such as calories. Returns null if an error occurs.
+ */
+export const newScrapeMenu = async (url) => {
+  try {
+    const { browser, page } = await initializeAndNavigate(url)
+
+    // Initialize weekMenu here to capture the URL and potentially other details
+    const weekMenu = { id: '', name: '', url, servings: {} }
+
+    // Extract the name and servings data within page.evaluate
+    const { name, servings } = await page.evaluate(() => {
+      const nutrients = {}
+      const buttonSelectors = Array.from(document.querySelectorAll('div.facts-container div.swiper-slide button'))
+      const menuName = document.querySelector('div.bg-beige h2.text-heading-sm')?.innerText.trim() || ''
+
+      for (const button of buttonSelectors) {
+        button.click()
+        // Awaiting here assumes the use of setTimeout or similar to wait for content update
+        const servingType = button.innerText.trim().toLowerCase()
+        const caloriesElement = Array.from(document.querySelectorAll('div.facts-container div.facts-container span')).find(element => element.textContent.includes('Calories'))
+        const factsRows = Array.from(document.querySelectorAll('div.facts-container div.facts-container span.font-bold'))
+        const servingNutrients = {}
+
+        servingNutrients.calories = caloriesElement?.nextSibling?.textContent.trim() || 'N/A' // Provide fallback
+
+        factsRows.forEach(row => {
+          const nutrientKey = row.innerText.trim().toLowerCase()
+          const nutrientValue = row.nextSibling ? row.nextSibling.textContent.trim().toLowerCase() : 'N/A' // Provide fallback
+          servingNutrients[nutrientKey] = nutrientValue
+        })
+
+        nutrients[servingType] = servingNutrients
+      }
+
+      return { name: menuName, servings: nutrients }
+    })
+
+    // Now, set the weekMenu properties based on the extracted data
+    weekMenu.name = name
+    weekMenu.servings = servings
+
+    // Extract ID and potentially adjust the name from the URL if necessary
+    const [id, ...nameParts] = url.split('/').pop().split('-')
+    weekMenu.id = id
+    if (!weekMenu.name) {
+      weekMenu.name = nameParts.join(' ').charAt(0).toUpperCase() + nameParts.join(' ').slice(1)
+    }
+
+    await browser.close()
+    return weekMenu
+  } catch (error) {
+    console.error(`Error scraping ${url}:`, error)
+    return null
+  }
+}
 
 /**
  * Scrapes menu data from a specified URL using Puppeteer. It navigates to the given URL,
@@ -18,12 +89,8 @@ import puppeteer from 'puppeteer'
  * name, URL, and servings data extracted from the nutrition facts table. Returns null if an error occurs.
  */
 export const scrapeMenu = async (url) => {
-  console.info(`Scraping menu data from ${url}`)
-  let browser
   try {
-    browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url, { waitUntil: 'networkidle2' })
+    const { browser, page } = await initializeAndNavigate(url)
 
     const data = await page.evaluate(() => {
       const servingTypes = Array.from(document.querySelectorAll('#nutrition-facts thead th')).slice(1).map(th => th.innerText.trim().toLowerCase())
@@ -50,12 +117,12 @@ export const scrapeMenu = async (url) => {
       }, {})
     })
 
+    await browser.close()
+
     const [id, ...name] = url.split('/').pop().split('-')
     return { id, name: name.join(' ').charAt(0).toUpperCase() + name.join(' ').slice(1), url, servings: data }
   } catch (error) {
     console.error(`Error scraping ${url}:`, error)
     return null
-  } finally {
-    await browser?.close()
   }
 }
