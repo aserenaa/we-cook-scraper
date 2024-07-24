ca21fc577f0efa1b6aa782da837ef435462c25e2
Abraham Serena <abraham.serena@hotmail.com>
2024-07-23 21:49:54 -0400

    add new scrapped data

--- scrapers/index.js
+++ scrapers/index.js
@@ -1,5 +1,4 @@
 export { scrapeMenuLinksByPeriod as factorScrapeMenuLinksByPeriod } from './factorMeals/scrapeMenuUrls.js'
 export { scrapeNutritionFacts as factorScrapeNutritionFacts } from './factorMeals/scrapeNutritionFacts.js'
-export { newScrapeMenu as newWeCookScrapeMenu, scrapeMenu as weCookScrapeMenu } from './weCookMeals/scrapeMenu.js'
+export { scrapeMenu as weCookScrapeMenu } from './weCookMeals/scrapeMenu.js'
 export { scrapeMenuLinksByDate as weCookScrapeMenuLinksByDate } from './weCookMeals/scrapeUrls.js'
-
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,4 @@
 export { buildUrlByDate, extractUrls } from './extractUrls.js'
 export { filterWeekMenus } from './filterWeekMenus.js'
-export { getMondaysOfMonth, isValidMonday } from './mondayFinder.js'
+export { getSundaysOfMonth } from './mondayFinder.js'
 export { initializeAndNavigate } from './puppeteerHelpers.js'
