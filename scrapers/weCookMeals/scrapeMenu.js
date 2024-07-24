83e2b4b22c26d2424c6ac59b749f1c071323e80a
Abraham Serena <abraham.serena@hotmail.com>
2024-07-23 21:49:02 -0400

    Update scrapeMenu.js to scrape menu data from a given WeCook URL

--- scrapers/weCookMeals/scrapeMenu.js
+++ scrapers/weCookMeals/scrapeMenu.js
@@ -1,7 +1,7 @@
 import { initializeAndNavigate } from '../../utils/index.js'
 
 /**
- * Scrapes menu data from a given URL.
+ * Scrapes menu data from a given WeCook URL.
  * It navigates to the URL, iterates through buttons within a swiper-slide container,
  * clicks each button, waits for the content to update, and extracts nutritional information.
  * |--------------------|
@@ -17,7 +17,7 @@ import { initializeAndNavigate } from '../../utils/index.js'
  * @returns {Promise<Object|null>} A promise that resolves to an object containing the meal's ID, name, URL, and servings data,
  * including nutritional facts such as calories. Returns null if an error occurs.
  */
-export const newScrapeMenu = async (url) => {
+export const scrapeMenu = async (url) => {
   try {
     const { browser, page } = await initializeAndNavigate(url)
 
@@ -57,62 +57,6 @@ export const newScrapeMenu = async (url) => {
 
     await browser.close()
     return weekMenu
-  } catch (error) {
-    console.error(`Error scraping ${url}:`, error)
-    return null
-  }
-}
-
-/**
- * Scrapes menu data from a specified URL using Puppeteer. It navigates to the given URL,
- * waits for the page to load, and then extracts menu information based on the structure
- * of the '#nutrition-facts' table within the page. This includes nutritional types and their
- * respective values. The method also parses the meal's ID and name from the URL.
- * | Nutrition Facts | Serving Size: Small  | Serving Size: Regular |
- * |-----------------|----------------------|-----------------------|
- * | Calories        | 100                  | 200                   |
- * | Fat             | 10g                  | 20g                   |
- * | ...             | ...                  | ...                   |
- * The function ensures the browser is closed after scraping is complete or if an error occurs,
- * to prevent resource leakage. In case of errors during scraping, it logs the error and returns null.
- *
- * @param {string} url - The URL of the week-menu page to scrape data from.
- * @returns {Promise<Object|null>} A promise that resolves to an object containing the meal's ID,
- * name, URL, and servings data extracted from the nutrition facts table. Returns null if an error occurs.
- */
-export const scrapeMenu = async (url) => {
-  try {
-    const { browser, page } = await initializeAndNavigate(url)
-
-    const data = await page.evaluate(() => {
-      const servingTypes = Array.from(document.querySelectorAll('#nutrition-facts thead th')).slice(1).map(th => th.innerText.trim().toLowerCase())
-      const rows = Array.from(document.querySelectorAll('#nutrition-facts tbody tr'))
-
-      return rows.reduce((acc, row) => {
-        const nutrientText = row.querySelector('td')?.innerText.trim().toLowerCase()
-        const nutrient = nutrientText
-          .replace(/\s*\/\s*\w+\s*:?/, '')
-          .replace(/\s+/g, '_')
-          .replace(/_+$/, '')
-          .replace(/:$/, '')
-          .replace(/_$/, '')
-
-        const measure = nutrientText?.match(/\/\s*(\w+)/)?.[1] || ''
-
-        servingTypes.forEach((type, index) => {
-          const value = row.querySelectorAll('td')[index + 1]?.innerText.trim()
-          acc[type] = acc[type] || {}
-          acc[type][nutrient] = `${value}${measure}`
-        })
-
-        return acc
-      }, {})
-    })
-
-    await browser.close()
-
-    const [id, ...name] = url.split('/').pop().split('-')
-    return { id, name: name.join(' ').charAt(0).toUpperCase() + name.join(' ').slice(1), url, servings: data }
   } catch (error) {
     console.error(`Error scraping ${url}:`, error)
     return null
